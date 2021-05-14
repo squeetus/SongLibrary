fd6209dc44917b21fe660ddd28b4dfa12d2a121e
Squeetus <burlinsond2@gmail.com>
2021-05-14 13:48:16 -0600

    endpoint and query to retrieve all songs

--- back/server.js
+++ back/server.js
@@ -6,13 +6,26 @@ const db = require('./db');
 const app = express();
 const port = 3000;
 
-const con = db.connect();
+db.connect();
 
 
 app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+app.get('/getAllSongs', (req, res) => {
+  songs = db.getAllSongs((err, songs) => {
+    if(err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    res.json(songs);
+  });
+
+});
+
 
 app.get('*', (req, res) => {
   res.statusCode = 200;
