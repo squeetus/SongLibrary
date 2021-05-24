0909afa583f9b8ad823b00fd17a58838bbbb0222
Squeetus <burlinsond2@gmail.com>
2021-05-24 12:01:23 -0600

    set a few e2e tests to ensure app works with database

--- front/song-library/e2e/src/app.e2e-spec.ts
+++ front/song-library/e2e/src/app.e2e-spec.ts
@@ -1,16 +1,36 @@
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 import { AppPage } from './app.po';
 
-describe('workspace-project App', () => {
+describe('song-library App', () => {
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', async () => {
+  it('should display the page title', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('song-library app is running!');
+    expect(await page.getTitle()).toEqual('SONG LIBRARY');
+  });
+
+  it('should display Song data in the table', async () => {
+    await page.navigateTo();
+    expect(await page.getTableRows()).toEqual(6);
+  });
+
+  it('should open the add modal on click', async () => {
+    await page.navigateTo();
+    await element(by.css('app-root add-modal .btn-primary')).click();
+    expect(await page.getOpenedAddModalHeader()).toEqual('Add a new Song');
+  });
+
+  it('should delete the first song in the table', async () => {
+    await page.navigateTo();
+    await element(by.css('app-root .mat-table delete-modal .btn-danger')).click();
+    await element(by.css('.modal-dialog .modal-footer .btn-danger')).click();
+    browser.driver.sleep(1000);
+    browser.waitForAngular();
+    expect(await page.getTableRows()).toEqual(5);
   });
 
   afterEach(async () => {
--- front/song-library/e2e/src/app.po.ts
+++ front/song-library/e2e/src/app.po.ts
@@ -5,7 +5,17 @@ export class AppPage {
     return browser.get(browser.baseUrl);
   }
 
-  async getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText();
+  async getTitle(): Promise<string> {
+    return element(by.css('app-root .headerBar h1')).getText();
+  }
+
+  async getTableRows(): Promise<number> {
+    return element.all(by.css('app-root .mat-table tr')).then((rows) => {
+      return rows.length;
+    });
+  }
+
+  async getOpenedAddModalHeader(): Promise<string> {
+    return element(by.css('.modal-dialog .modal-header h5')).getText();
   }
 }
